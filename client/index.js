68a21cc6da0f2ee75f1ff5e845e14214410dbb63
Johan Greus <jgreus@gmail.com>
2018-11-24 13:56:01 +0100

    Client web socket connection

--- client/index.js
+++ client/index.js
@@ -1,1 +1,23 @@
-console.log("hello")
+var stagecast = stagecast || {
+    getUserId: function () {
+        return "1234"
+    }
+};
+
+// Create WebSocket connection.
+const socket = new WebSocket('wss://stagecast.se/api/events/hypeisland/ws');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    console.log(stagecast);
+    var userId = stagecast.getUserId();
+    var json = JSON.stringify({
+        userId: stagecast.getUserId()
+    });
+    socket.send(json);
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
